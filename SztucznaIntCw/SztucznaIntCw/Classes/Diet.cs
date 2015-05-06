cd129738886a6ae1d6d61aa5865671a1b251cf45
robson081192 <robson081192@gmail.com>
2015-05-06 13:54:56 +0200

    Macierze kurwa:D

--- SztucznaIntCw/SztucznaIntCw/Classes/Diet.cs
+++ SztucznaIntCw/SztucznaIntCw/Classes/Diet.cs
@@ -5,6 +5,7 @@ using System.Windows.Forms;
 using SztucznaIntCw.Classes.Interfaces;
 using SztucznaIntCw.Classes.NonAbstract;
 using SztucznaIntCw.Enums;
+using MatrixCalculator;
 
 namespace SztucznaIntCw.Classes
 {
@@ -33,13 +34,65 @@ namespace SztucznaIntCw.Classes
                 getMacroElementForCurrentMeal(MainSourceOf.Carbs, meal.Key, _carbsProducts);
                 getMacroElementForCurrentMeal(MainSourceOf.Fat, meal.Key, _fatProducts);
 
-                composeMeal(_proteinProducts, meal.Value);
-                composeMeal(_carbsProducts, meal.Value);
-                composeMeal(_fatProducts, meal.Value);
+                //composeMeal(_proteinProducts, meal.Value);
+                //composeMeal(_carbsProducts, meal.Value);
+                //composeMeal(_fatProducts, meal.Value);
+                getMeal(meal.Value, _proteinProducts, _carbsProducts, _fatProducts);
 
             }
             
             
+        }
+
+        private void getMeal(IMeal meal, List<IProduct> protProducts, List<IProduct> carbProducts,
+            List<IProduct> fatProducts)
+        {
+            float[,] macroElements = new float[3,3];
+            macroElements[0, 0] = (float) protProducts[0].Protein;
+            macroElements[0, 1] = (float) carbProducts[0].Protein;
+            macroElements[0, 2] = (float) fatProducts[0].Protein;
+
+            macroElements[1, 0] = (float) protProducts[0].Carbs;
+            macroElements[1, 1] = (float) carbProducts[0].Carbs;
+            macroElements[1, 2] = (float) fatProducts[0].Carbs;
+
+            macroElements[2, 0] = (float) protProducts[0].Fat;
+            macroElements[2, 1] = (float) carbProducts[0].Fat;
+            macroElements[2, 2] = (float) fatProducts[0].Fat;
+
+            D2Matrix constValues = new D2Matrix(macroElements);
+
+            float[] macroElementsTotal = new float[]
+            {
+                (float)meal.TotalGramsOfProteins,
+                (float)meal.TotalGramsOfCarbs,
+                (float)meal.TotalGramsOfFats
+            };
+
+            float matrixDet = constValues.GetDeterminantSquareMatrix();
+            macroElements = constValues.GetComplementMatrix(matrixDet);
+            constValues = new D2Matrix(macroElements);
+            
+            constValues.InverseMatrix(matrixDet);
+            constValues.MatrixTransposition();
+
+            float[] resultArray = new float[3];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    resultArray[i] = constValues[i, j]*macroElementsTotal[j];
+                }
+            }
+
+            meal.MealProducts.Add(protProducts[0], (decimal)resultArray[0]);
+            meal.MealProducts.Add(carbProducts[0], (decimal)resultArray[1]);
+            meal.MealProducts.Add(fatProducts[0], (decimal)resultArray[2]);
+
+            protProducts.RemoveAt(0);
+            carbProducts.RemoveAt(0);
+            fatProducts.RemoveAt(0);
+
         }
 
         private void getMacroElementForCurrentMeal(MainSourceOf macroElement, int mealNumber, List<IProduct> destinationList)
